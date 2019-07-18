55573e0e3bddde60d1167f70639cb924114f5ecc
MacRat <m@crat.jp>
2019-07-18 17:04:22 +0900

    Added deploy settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,14 @@
+const router = process.env.DEPLOY_ENV !== 'GH_PAGES' ? {} : {
+	base: '/trans-qr/',
+};
+
 export default {
     head: {
         title: 'TransQR',
+		meta: [
+			{charset: 'utf-8'},
+			{name: 'viewport', content: 'width=device-width, initial-scale=1'},
+		],
         link: [
             {rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.9.0/css/all.css'},
         ],
@@ -11,4 +19,5 @@ export default {
     modules: [
         '@nuxtjs/dotenv',
     ],
+    router: router,
 }
